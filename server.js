18b8e684615729948dc7dca408d159283dced69b
David Shalom <david.shalom@bskyb.com>
2016-02-13 22:33:47 +0000

    wip

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+  id: 1,
+  description: 'go to bed',
+  completed: false
+},
+{
+  id: 2,
+  description: 'feed guinee pigs',
+  completed: false
+},
+{
+  id: 3,
+  description: 'get some food',
+  completed: true
+}];
+
 app.get('/', function(req, res){
   res.send('todo root');
+});
+
+app.get('/todos', function(req, res){
+  res.json(todos);
 });
 
 app.listen(PORT, function(){
