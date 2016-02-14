2ac31c5982650670c2b9033e59c6034a8c9e1fce
David Shalom <david.shalom@bskyb.com>
2016-02-14 08:02:51 +0000

    refactor

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParse = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -16,8 +18,8 @@ app.get('/todos', function(req, res){
 });
 
 app.get('/todos/:id', function(req, res){
-  var id = parseInt(req.params.id);
-  var matchedToDo;
+  var todoId = parseInt(req.params.id, 10);
+  var matchedToDo = _.findWhere(todos, {id: todoId});
 
   todos.forEach(function(todo){
     if(todo.id === id){
@@ -32,7 +34,13 @@ app.get('/todos/:id', function(req, res){
 });
 
 app.post('/todos', function(req, res){
-  var body = req.body;
+  var body = _.pick(req.body, 'description',  'completed');
+
+  if(!_.isBoolean(body.completed) || !_.isString(body.description)  || body.description.trim().length === 0 ){
+      res.status(400).send();
+      return;
+  }
+  body.description = body.description.trim();
   body.id = nextToDoId++;
   res.json(body);
   todos.push(body);
