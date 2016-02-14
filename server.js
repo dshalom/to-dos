b331241994bb234fe451147eaf7a9db2750d6b6a
David Shalom <david.shalom@bskyb.com>
2016-02-14 00:00:06 +0000

    added get item

--- server.js
+++ server.js
@@ -26,6 +26,34 @@ app.get('/todos', function(req, res){
   res.json(todos);
 });
 
+function findToDo(id){
+    var r = todos.reduce(function(acc, value){
+    if(value.id === id){
+     acc.push(value);
+    }
+    return acc;
+
+}, []);
+    return r[0];
+}
+
+
+app.get('/todos/:id', function(req, res){
+  var id = parseInt(req.params.id);
+  var matchedToDo;
+
+  todos.forEach(function(todo){
+    if(todo.id === id){
+      matchedToDo = todo;
+    }
+  });
+  if(matchedToDo){
+    res.send(res.json(matchedToDo));
+  }else{
+    res.status(404).send();
+  }
+});
+
 app.listen(PORT, function(){
   console.log('App listening on port ' + PORT);
 });
